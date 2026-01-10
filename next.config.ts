7e93b121340a0646c0dc4875eaa3109c33f47be8
Jayakrishnan <mail@iamjk.in>
2026-01-10 20:45:46 +0530

    Revert "Update next.config.ts"
    This reverts commit ca16df372e4cf583d97a72728d6c5d320f4273ad.

--- next.config.ts
+++ next.config.ts
@@ -22,9 +22,19 @@ const nextConfig: NextConfig = {
         hostname: "via.placeholder.com",
       },
       {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "3000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000", // Assuming API might be here just in case
         pathname: "/**",
       },
       {
