700806d62faaf7df68e1db68e08a3b46d5525db7
adharshkc <adharshkc2017@gmail.com>
2026-01-22 11:15:14 +0530

    fixed mobile navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -320,8 +320,8 @@ const Navbar = () => {
 
           {/* MOBILE SEARCH BAR */}
           <div
-            className={`lg:hidden mt-2 pb-3 overflow-hidden transition-[max-height,opacity] duration-300 ease-in-out ${
-              showMobileSearch ? "opacity-100 max-h-[60px]" : "opacity-0 max-h-0"
+            className={`lg:hidden mt-2 overflow-hidden transition-[max-height,opacity] duration-300 ease-in-out ${
+              showMobileSearch ? "opacity-100 max-h-[60px] pb-3" : "opacity-0 max-h-0 pb-0"
             }`}
           >
             <div className="relative h-12">
