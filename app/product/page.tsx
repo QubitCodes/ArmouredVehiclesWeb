204f636001614b83dd21c7d39cca28c4b2b95c51
Jayakrishnan <mail@iamjk.in>
2026-02-04 12:50:10 +0530

    /product goes to /products

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,24 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import DesktopLayout from "./components/desktop/DesktopLayout";
-import MobileLayout from "./components/mobile/MobileLayout";
-
-
-
-export default function ProductDetailsPage() {
-  return (
-    <>
-    <section className="bg-[#F0EBE3] pt-[48px] md:pt-0">
-  <div className="md:hidden">
-    <MobileLayout />
-  </div>
-
-  <div className="hidden md:block">
-    <DesktopLayout />
-  </div>
-</section>
-
-
-    </>
-  );
+export default function Products() {
+  redirect("/products");
 }
