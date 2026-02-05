43a63ef386d819927833eb968c6e1e9f8d2e83e8
Jayakrishnan <mail@iamjk.in>
2026-02-06 00:17:18 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -50,7 +50,7 @@ const nextConfig: NextConfig = {
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: true,
+    unoptimized: true, // Comment in production
   },
 };
 
